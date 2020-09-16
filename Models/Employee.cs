97f3fcaf04b3117ba8461ec271de43496087561e
Sebastian Bobrowski <sebastian.bobrowski@outlook.com>
2020-09-16 11:53:27 +0200

    added job title property of type enum to employee model

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -2,7 +2,14 @@ public class Employee {
     public long Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
+    public EmployeeJobTitles JobTitle { get; set; }
 
     public long CompanyId { get; set; }
     public Company Company { get; set; }
+}
+public enum EmployeeJobTitles {
+    Administrator,
+    Developer,
+    Architect,
+    Manager
 }
