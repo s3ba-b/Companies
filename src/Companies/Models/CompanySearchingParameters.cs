45cd145e6a592146e8b094c3cf39026c40244621
Sebastian Bobrowski <sebastian.bobrowski@outlook.com>
2020-09-18 15:11:33 +0200

    Update CompanySearchingParameters Model

--- src/Companies/Models/CompanySearchingParameters.cs
+++ src/Companies/Models/CompanySearchingParameters.cs
@@ -4,7 +4,9 @@ namespace TestBackendDeveloper.Models
 {
     public class CompanySearchingParameters
     {
-        public string Keyword { get; set; }
+        #nullable enable
+        public string? Keyword { get; set; }
+        #nullable disable
         public DateTime? EmployeeDateOfBirthFrom { get; set; }
         public DateTime? EmployeeDateOfBirthTo { get; set; }
         public EmployeeJobTitle? EmployeeJobTitles { get; set; }
